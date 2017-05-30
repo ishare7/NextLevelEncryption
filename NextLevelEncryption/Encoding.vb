564f9874729a08e80f8cd29f8476960684c6d74e
ishare7 <i_share7@live.com>
2017-05-29 20:31:16 -0400

    NextLevelEncryption v1.2.5 Beta Release
    Features:
    • AES Encryption for text.
    • AES Encryption for files (ransomware protection)
    • Double AES Encryption for text:
    Encrypts the hash key using SHA1 and MD5 encryption.
    • Base64 Encryption for files.

---
+++ NextLevelEncryption/Encoding.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Text.Encoding
+Public Module Encoding
+    ''' <summary>
+    ''' Converts an input string to binary
+    ''' </summary>
+    ''' <param name="input">String to convert to binary</param>
+    ''' <returns>Returns input string to binary</returns>
+    ''' <remarks></remarks>
+    Public Function StringToBinary(input As String) As String
+        Dim Value As String = Nothing
+        Dim Re As New System.Text.StringBuilder
+        For Each Character As Byte In System.Text.ASCIIEncoding.ASCII.GetBytes(input)
+            Re.Append(System.Convert.ToString(Character, 2).PadLeft(8, "0"))
+        Next
+
+        Value = Re.ToString.Substring(0, Re.ToString.Length - 0)
+        Return Value
+    End Function
+    ''' <summary>
+    ''' Converts input binary to string
+    ''' </summary>
+    ''' <param name="input">Binary input to covert back to string</param>
+    ''' <returns>Returns binary input converted to string</returns>
+    ''' <remarks></remarks>
+    Public Function BinaryToString(input As String) As String
+        Try
+            Dim Values As String = Nothing
+            Dim Character As String = System.Text.RegularExpressions.Regex.Replace(input, "[^01]", "")
+            Dim ByteArrays((Character.Length / 8) - 1) As Byte
+            For Index As Integer = 0 To ByteArrays.Length - 1
+                ByteArrays(Index) = System.Convert.ToByte(Character.Substring(Index * 8, 8), 2)
+            Next
+            Values = System.Text.ASCIIEncoding.ASCII.GetString(ByteArrays)
+            Return Values
+        Catch ex As Exception
+            Throw New ArgumentException("Error! Please provide valid binary characters!")
+        End Try
+    End Function
+    ''' <summary>
+    ''' Encrypt a file with Base64
+    ''' </summary>
+    ''' <param name="file">File to encrypt</param>
+    ''' <remarks></remarks>
+    Public Sub Base64Encrypt(file As String)
+        IO.File.WriteAllText(file, System.Convert.ToBase64String(IO.File.ReadAllBytes(file), Base64FormattingOptions.InsertLineBreaks))
+    End Sub
+    ''' <summary>
+    ''' Decrypt a file with Base64
+    ''' </summary>
+    ''' <param name="file">File to decrypt</param>
+    ''' <remarks></remarks>
+    Public Sub Base64Decrypt(file As String)
+        IO.File.WriteAllBytes(file, System.Convert.FromBase64String(IO.File.ReadAllText(file)))
+    End Sub
+    ''' <summary>
+    ''' Encodes a string from one encoding to another.
+    ''' </summary>
+    ''' <param name="input">String to encode</param>
+    ''' <param name="original">Input string encoding</param>
+    ''' <param name="output">Output string encoding</param>
+    ''' <returns>Returns input string encoded into output encoding</returns>
+    ''' <remarks></remarks>
+    Public Function Encode(input As String, original As Text.Encoding, output As Text.Encoding) As String
+        Return output.GetString(original.GetBytes(input))
+    End Function
+    ''' <summary>
+    ''' Encodes a string from one encoding to another.
+    ''' </summary>
+    ''' <param name="input">String to encode</param>
+    ''' <param name="original">Input string encoding</param>
+    ''' <param name="output">Output string encoding</param>
+    ''' <returns>Returns input string encoded into output encoding</returns>
+    ''' <remarks></remarks>
+    Public Function Encode(input As Char(), original As Text.Encoding, output As Text.Encoding) As String
+        Return output.GetString(original.GetBytes(input))
+    End Function
+    ''' <summary>
+    ''' Encodes a string from one encoding to another.
+    ''' </summary>
+    ''' <param name="input">String to encode</param>
+    ''' <param name="original">Input string encoding</param>
+    ''' <param name="output">Output string encoding</param>
+    ''' <returns>Returns input string encoded into output encoding</returns>
+    ''' <remarks></remarks>
+    Public Function Encode(input As Byte(), original As Text.Encoding, output As Text.Encoding) As String
+        Return output.GetString(input)
+    End Function
+    ''' <summary>
+    ''' Encodes an array of strings from one encoding to another.
+    ''' </summary>
+    ''' <param name="input">String array to encode</param>
+    ''' <param name="original">Input string encoding</param>
+    ''' <param name="output">Output string encoding</param>
+    ''' <returns>Returns input string encoded into output encoding</returns>
+    ''' <remarks></remarks>
+    Public Function Encode(input As String(), original As Text.Encoding, output As Text.Encoding) As String()
+        Dim list As New List(Of String)
+        For Each s As String In input
+            list.Add(output.GetString(original.GetBytes(s)))
+        Next
+        Return list.ToArray
+    End Function
+End Module
---
+++ NextLevelEncryption/Encryption.vb
@@ -0,0 +1,183 @@
+﻿Public Module Encryption
+    ''' <summary>
+    ''' Encrypt text using AES Algorithm
+    ''' </summary>
+    ''' <param name="text">Text to encrypt</param>
+    ''' <param name="password">Password with which to encrypt</param>
+    ''' <returns>Returns encrypted text</returns>
+    ''' <remarks></remarks>
+    Public Function Encrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim encrypted As String = ""
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
+            Dim Buffer As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(text)
+            encrypted = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            Return encrypted
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+    ''' <summary>
+    ''' Decrypt text using AES Algorithm
+    ''' </summary>
+    ''' <param name="text">Text to decrypt</param>
+    ''' <param name="password">Password with which to decrypt</param>
+    ''' <returns>Returns decrypted text</returns>
+    ''' <remarks></remarks>
+    Public Function Decrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim decrypted As String = ""
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = Convert.FromBase64String(text)
+            decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            Return decrypted
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+    ''' <summary>
+    ''' Encrypt file to protect from hackers asking ransom
+    ''' </summary>
+    ''' <param name="file">File to Ransomware Protect</param>
+    ''' <param name="key">Password to encrypt with</param>
+    ''' <remarks></remarks>
+    Public Sub CryptoRansomwareEncryption(ByVal file As String, ByVal key As String)
+        If My.Computer.FileSystem.FileExists(file) Then
+            My.Computer.FileSystem.WriteAllBytes(file, Bytes_AES_Encrypt(My.Computer.FileSystem.ReadAllBytes(file), key), False)
+        End If
+    End Sub
+    ''' <summary>
+    ''' Decrypt file from protection from hackers asking ransom
+    ''' </summary>
+    ''' <param name="file">File to Ransomware Unprotect</param>
+    ''' <param name="key">Password to decrypt with</param>
+    ''' <remarks></remarks>
+    Public Sub CryptoRansomwareDecryption(ByVal file As String, ByVal key As String)
+        If My.Computer.FileSystem.FileExists(file) Then
+            My.Computer.FileSystem.WriteAllBytes(file, Bytes_AES_Decrypt(My.Computer.FileSystem.ReadAllBytes(file), key), False)
+        End If
+    End Sub
+    Private Function Bytes_AES_Encrypt(ByVal input As Byte(), ByVal key As String) As Byte()
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim SHA256 As New System.Security.Cryptography.SHA256Cng
+        Dim ciphertext As String = ""
+        Try
+            AES.Key = SHA256.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(key))
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
+            Dim Buffer As Byte() = input
+            Return DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length)
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+    Private Function Bytes_AES_Decrypt(ByVal input As Byte(), ByVal key As String) As Byte()
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim SHA256 As New System.Security.Cryptography.SHA256Cng
+        Try
+            AES.Key = SHA256.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(key))
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = input
+            Return DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length)
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+    ''' <summary>
+    ''' Super Encrypt text using AES Algorithm and SHA1 Algorithm
+    ''' </summary>
+    ''' <param name="text">Text to encrypt</param>
+    ''' <param name="password">Password with which to encrypt</param>
+    ''' <returns>Returns encrypted text</returns>
+    ''' <remarks></remarks>2
+    Public Function SuperEncrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Dim encrypted As String = ""
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
+            Dim Buffer As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(text)
+            encrypted = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+        Dim Hash_SHA1 As New System.Security.Cryptography.SHA1CryptoServiceProvider
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_SHA1.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
+            Dim Buffer As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(encrypted)
+            encrypted = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            Return encrypted
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+    ''' <summary>
+    ''' Super Decrypt text using AES Algorithm and SHA1 Algorithm
+    ''' </summary>
+    ''' <param name="text">Text to decrypt</param>
+    ''' <param name="password">Password with which to decrypt</param>
+    ''' <returns>Returns decrypted text</returns>
+    ''' <remarks></remarks>
+    Public Function SuperDecrypt(text As String, password As String) As String
+        Dim AES As New System.Security.Cryptography.RijndaelManaged
+        Dim Hash_AES As New System.Security.Cryptography.SHA1CryptoServiceProvider
+        Dim decrypted As String = ""
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = Convert.FromBase64String(text)
+            decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+        Dim Hash_SHA1 As New System.Security.Cryptography.MD5CryptoServiceProvider
+        Try
+            Dim hash(31) As Byte
+            Dim temp As Byte() = Hash_SHA1.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+            Array.Copy(temp, 0, hash, 0, 16)
+            Array.Copy(temp, 0, hash, 15, 16)
+            AES.Key = hash
+            AES.Mode = Security.Cryptography.CipherMode.ECB
+            Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = Convert.FromBase64String(decrypted)
+            decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            Return decrypted
+        Catch ex As Exception
+            Throw New ArgumentException(ex.Message)
+        End Try
+    End Function
+End Module
